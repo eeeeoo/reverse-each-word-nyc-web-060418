0bde8f33f25dcc20da1d84d9d2f9cab213ef5f56
Jee Yoon Lee <jlee66@wellesley.edu>
2018-06-04 21:22:58 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+  reversed = []
+  new_array = string.split(" ")
+  new_array.each do |element|
+    reversed << element.reverse
+  end
+  return reversed.join(" ")
+end
+
+def reverse_each_word(string)
+  reversed = []
+  new_array = string.split(" ")
+  new_array.collect do |element|
+    reversed << element.reverse
+  end
+  return reversed.join(" ")
+end
